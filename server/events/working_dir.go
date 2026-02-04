7704a47585ab292adbd080487d604525508115d8
Gavin Williams <fatmcgav@gmail.com>
2026-02-04 15:17:03 +0000

    fix: Prevent un-necessary re-cloning of repo
    This commit updates the `remoteHasBranch` logic to check for the remote
    branch using `git ls-remote` rather than `git show-ref` as the latter
    will fail when using the `branch` checkout strategy.
    
    Also add a supporting test case.
    
    Disclaimer: Gemini helped create some of this code.
    
    Fixes: #6146
    
    Signed-off-by: Gavin Williams <fatmcgav@gmail.com>

--- server/events/working_dir.go
+++ server/events/working_dir.go
@@ -262,11 +262,18 @@ func (w *FileWorkspace) HasDiverged(logger logging.SimpleLogging, cloneDir strin
 }
 
 func (w *FileWorkspace) remoteHasBranch(logger logging.SimpleLogging, c wrappedGitContext, branch string) bool {
-	ref := "refs/remotes/origin/" + branch
-
-	err := w.wrappedGit(logger, c, "show-ref", "--verify", ref)
+	// We use ls-remote to check if the branch exists on the remote.
+	// We use --exit-code which will return exit code 2 if the branch
+	// doesn't exist and 0 if it does.
+	err := w.wrappedGit(logger, c, "ls-remote", "--exit-code", "--heads", "origin", fmt.Sprintf("refs/heads/%s", branch))
 	if err != nil {
-		logger.Warn("remote-tracking branch %s not found locally", ref)
+		// Distinguish "no matching refs" (exit code 2) from other git errors such as
+		// transport or authentication problems so logs are not misleading.
+		if strings.Contains(err.Error(), "exit status 2") {
+			logger.Warn("could not find branch %q on remote 'origin': %s", branch, err)
+		} else {
+			logger.Warn("checking for branch %q on remote 'origin': %s", branch, err)
+		}
 		return false
 	}
 
